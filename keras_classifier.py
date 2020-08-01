11025a4172850f71381133a5c7361db20f936360
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2020-08-01 09:29:02 -0400

    documenting the classifer

--- keras_classifier.py
+++ keras_classifier.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import math
 
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
@@ -13,33 +14,49 @@ from keras import callbacks
 kinda like a jupyter notebook, but without the notebook
 '''
 
+'''
+get features and labels values from a csv
+a puzzle is usually a 3 by 3 matrix (3,3), but it has been written to a csv as a single row (9,)
+'''
 dataset = pd.read_csv('samples-tictactoe.csv')
 X = dataset.iloc[:, 0:9].values
 y = dataset.iloc[:, 9:10].values
 
+'''
+our features are categorial, so encode them.. 
+this transforms the data from (9,) to (27,)
+'''
 encoder_board = OneHotEncoder()
 X = encoder_board.fit_transform(X).toarray()
 
+# we need to separate some data to train with, and reserve some to test on
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
+# let's use validation data to support early stopping
 offset = -(len(X_train)//5)
 X_val = X_train[offset:]
 y_val = y_train[offset:]
 X_train = X_train[:offset]
 y_train = y_train[:offset]
 
+# here begins our neural network
 model = Sequential()
 
+# we have an input layer of 27
+# we have 3 hidden layers of 14
 model.add(Dense(units=14, kernel_initializer='uniform',
                 activation='relu', input_dim=27))
 model.add(Dense(units=14, kernel_initializer='uniform',
                 activation='relu'))
 model.add(Dense(units=14, kernel_initializer='uniform',
                 activation='relu'))
-
+# we have an output layer of 1: this is a binary classifier: its either a vlaid tic tac toe board, or it is not.
 model.add(Dense(units=1, kernel_initializer='uniform', activation='sigmoid'))
 
+'''
+we want to train our network by modifying the internal weights based on some loss: binary crossentropy is perfectly fit for a binary classifier. for determining how we use our loss, we use adam (this is the go-to method in practice generally).
+'''
 model.compile(optimizer='adam', loss='binary_crossentropy',
               metrics=['accuracy'])
 
@@ -51,11 +68,20 @@ epochs = 500
 earlystopping = callbacks.EarlyStopping(monitor="val_accuracy",
                                         mode="max", patience=epochs//100,
                                         restore_best_weights=True)
-
-model.fit(X_train, y_train, batch_size=10,
+'''
+the batch size was 10 by default. it is suggested to decrease batch size as much as possible for parallelism in gpu training, but lower batch sizes won't globally optimize as well.
+in our case, we know some things about the sample data. I generated it with of-each, generating three different classes of data: fully random, sparse random, and random but valid. so we will need at least a batch_size large enough to allow each batch a chance of reflecting 3 states. that size grows with the log of the training data length
+'''
+batch_size = 3 * (int(math.log(len(X_train))) + 1)
+print('batch_size', batch_size)
+# train the data, yeay!
+model.fit(X_train, y_train, batch_size=batch_size,
           epochs=epochs, callbacks=[earlystopping], validation_data=(X_val, y_val))
+
+# predict the results. double-yeay!
 y_pred = (model.predict(X_test) > 0.5).astype("int32")
 
+# let the user know that something happened, because this is not a jupyter notebook
 index = 0
 miss = 0
 for received, expected in list(zip(y_test, y_pred)):
